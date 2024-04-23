21562790a0eec4757e1efd943fcec8876fadc0a3
mihnearad <it@rad-limited.com>
2024-04-23 16:45:53 +0000

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react' 
+import Navbar from './Components/NavBar/Navbar'
+import Hero from './Components/Hero/Hero'
+import About from './Components/About/About'
+import Projects from './Components/Projects/Projects'
+import MyWork from './Components/MyWork/MyWork'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
+ 
+ const App = () => {
+  return (
+    <div>
+      
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
+
+      </div>
+
+  )
+}
+
+export default App  
---
+++ src/Components/Projects/projects_data.js
@@ -0,0 +1,25 @@
+const Projects_Data = [
+  {
+    s_no: "01",
+    s_name: "AVA - Automated Virtual Assistant",
+    s_desc: "AVA is an AI-powered chatbot developed with the aim to...",
+  },
+  {
+    s_no: "02",
+    s_name: "Parking Analyzer",
+    s_desc:
+      "This tool emerged from a blend of my interests in Python and PostgreSQL. If you've ever lived in a crowded area, you know...",
+  },
+  {
+    s_no: "03",
+    s_name: "Security Cameras",
+    s_desc: "Web development is the process of building, programming...",
+  },
+  {
+    s_no: "04",
+    s_name: "Security Assesment",
+    s_desc: "Web development is the process of building, programming...",
+  },
+];
+
+export default Projects_Data;
